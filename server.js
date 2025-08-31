ebebc288c88db8e396c1feb33822c06e463c45b0
panuwat-ta <panuwattakham2002@gmail.com>
2025-08-31 22:41:36 +0700

    helmet - เพิ่มเกราะป้องกัน

--- server.js
+++ server.js
@@ -2,13 +2,16 @@
 const express = require('express');
 require('dotenv').config(); // << เพิ่มบรรทัดนี้ที่ด้านบน
 const cors = require('cors'); // << Import cors
+const helmet = require('helmet'); // << Import helmet
 
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000; // << อ่านค่า PORT จาก .env
 const APP_NAME = process.env.APP_NAME;
 
+app.use(helmet()); // << เพิ่มบรรทัดนี้: ใส่เกราะป้องกัน!
 app.use(cors());  // << เพิ่มบรรทัดนี้: ใช้ cors กับทุก request
+
 
 app.get('/', (req, res) => {
   res.send(`<h1>Hello from ${APP_NAME}!</h1>`);
