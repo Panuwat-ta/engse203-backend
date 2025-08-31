4977ceec07a8913f34de444ce86678d8feada785
panuwat-ta <panuwattakham2002@gmail.com>
2025-08-31 22:35:51 +0700

    cors - เปิดประตูให้ Frontend

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 // server.js
 const express = require('express');
 require('dotenv').config(); // << เพิ่มบรรทัดนี้ที่ด้านบน
+const cors = require('cors'); // << Import cors
 
 const app = express();
+require('dotenv').config();
 const PORT = process.env.PORT || 3000; // << อ่านค่า PORT จาก .env
 const APP_NAME = process.env.APP_NAME;
 
+app.use(cors());  // << เพิ่มบรรทัดนี้: ใช้ cors กับทุก request
+
 app.get('/', (req, res) => {
   res.send(`<h1>Hello from ${APP_NAME}!</h1>`);
+});
+
+// เพิ่ม Route ใหม่สำหรับทดสอบ
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'This data is open for everyone!' });
 });
 
 app.listen(PORT, () => {
