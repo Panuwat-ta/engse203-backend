797bd6add2b173a9e88b0cf4a080584c5d88c768
Panuwat-ta <panuwattakham2002@gmail.com>
2025-10-05 02:52:14 +0700

    add Auto-Update Feature

--- LAB8_Cross-Platform_Development/agent-wallboard/main.js
+++ LAB8_Cross-Platform_Development/agent-wallboard/main.js
@@ -1,6 +1,23 @@
 const { app, BrowserWindow, ipcMain, Tray, Menu, Notification, dialog } = require('electron');
 const path = require('path');
 const fs = require('fs');
+const { autoUpdater } = require('electron-updater');
+const isDev = !app.isPackaged;
+
+
+// Auto-updater setup (เฉพาะ production)
+if (!isDev) {
+  autoUpdater.checkForUpdatesAndNotify();
+  
+  autoUpdater.on('update-available', () => {
+    console.log('Update available');
+  });
+  
+  autoUpdater.on('update-downloaded', () => {
+    console.log('Update downloaded');
+    autoUpdater.quitAndInstall();
+  });
+}
 
 let mainWindow, tray;
 
